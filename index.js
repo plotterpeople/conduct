fe3745b67d8167958cec0566644969d25add257b
Jeremy Apthorp <nornagon@nornagon.net>
2019-01-10 21:38:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(require('cors')())
 app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: false }))
 
-const mentions = ' <@visnup> <@billyroh> <@nexxy> <@kellyk> <@thealphanerd>'
+const mentions = ' <@nornagon> <@Kyle Stetz>'
 app.use(function (req, res) {
   let body = req.body.Body || req.body.body || req.body.description
 
